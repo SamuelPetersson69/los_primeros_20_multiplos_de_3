8891bda54b36b972f967affff0b10becc3474b78
samuela <samuel.petersson@udla.edu.ec>
2023-10-12 23:46:41 -0500

    elimine los comentarios

--- suma_gauss.c
+++ suma_gauss.c
@@ -3,19 +3,16 @@
 int main() {
     int numero, suma = 0;
 
-    //mejor puse las variables completas como en ves de sum suma etc
-    // Solicita un numero entero para evitar decimales etc 
+
     printf("Ingrese un número entero: ");
     scanf("%d", &numero);
 
-    // esto calcula el resultado
+
     for (int i = 1; i <= numero; ++i) {
         suma += i;
     }
-     // aqui simplemente te imprime la respuesta
+
     printf("La suma de Gauss hasta %d es: %d\n", numero, suma);
-
-
 
     return 0;
 }
