54c1c47ad5fbfaed3a67711e8bbcd676e6cd08f8
samuel <samuel.petersson@udla.edu.ec>
2023-10-16 22:14:59 -0500

    cuarto y ultimo repositorio del dia de hoy

--- leer_un_num_entero_y_verificar_si_es_primo.c
+++ leer_un_num_entero_y_verificar_si_es_primo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// esta linea tiene la funcion de determinar li es numero primo.,g
+
 bool esPrimo(int num) {
     if (num <= 1) {
         return false;
@@ -12,3 +12,26 @@ bool esPrimo(int num) {
         }
     }
     return true;
+}
+
+int main() {
+    int numero, sumaDigitos = 0;
+
+
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &numero);
+
+    int tempNumero = numero;
+    while (tempNumero != 0) {
+        sumaDigitos += tempNumero % 10;
+        tempNumero /= 10;
+    }
+
+    if (esPrimo(sumaDigitos)) {
+        printf("La suma de los digitos %d es un numero primo.\n", sumaDigitos);
+    } else {
+        printf("La suma de los digitos %d no es un numero primo.\n", sumaDigitos);
+    }
+
+    return 0;
+}
